71b41eaeb3b211883d2e49399f49a2f13cc3798d
Nikita Bondarev <nikita.bondarev1807@gmail.com>
2022-08-23 03:42:21 +0300

    Upgrade index, styles, add jQuery and galleryPlugin

---
+++ galleryPlugin.js
@@ -0,0 +1,32 @@
+const animationPanding = () => {
+  const currentPaging = $('.pointer.active');
+  const currentPagingIndex = $('.pointer.active').index();
+  const nextPagingIndex = currentPagingIndex + 1;
+  const nextPaging = $('.pointer').eq(nextPagingIndex);
+  currentPaging.removeClass('active');
+
+  if (nextPagingIndex === ($('.pointer:last').index() + 1)) {
+    $('.pointer').eq(0).addClass('active');
+  } else {
+    nextPaging.addClass('active')
+  }
+}
+
+$('.button-gallery').click(function () {
+  const currentImage = $('.img.active');
+  const currentImageIndex = $('.img.active').index();
+  const nextImageIndex = currentImageIndex + 1;
+  const nextImage = $('.img').eq(nextImageIndex);
+  currentImage.fadeOut(200);
+  currentImage.removeClass('active');
+
+  if (nextImageIndex === ($('.img:last').index() + 1)) {
+    $('.img').eq(0).fadeIn(1500);
+    $('.img').eq(0).addClass('active');
+  } else {
+    nextImage.fadeIn(1500);
+    nextImage.addClass('active')
+  }
+
+  animationPanding();
+})
